9e4b6f2669693aa2645fd08d7622da59f7ce6ec2
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-06-02 16:05:12 -0700

    Updates

---
+++ precache-manifest.00de0390097d1a525fdfb060a6ee6c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db7a5d7dac0c86595fa579a81d0bf69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a347406ef92a1347cf6",
+    "url": "/static/css/main.4e36bd0a.chunk.css"
+  },
+  {
+    "revision": "c0e2e0a2b449e95dbac1",
+    "url": "/static/js/2.fe024166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe024166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a347406ef92a1347cf6",
+    "url": "/static/js/main.777ab116.chunk.js"
+  },
+  {
+    "revision": "049b83375d1da9f455c1",
+    "url": "/static/js/runtime-main.bdace896.js"
+  }
+]);
