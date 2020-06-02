2629a3c28bdeabcef4ebb626bdea37621dc76606
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-06-02 14:47:58 -0700

    circles component  pre-commit

--- src/Components/Circles/Circles.js
+++ src/Components/Circles/Circles.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Circles = (props) => {
     return(
         <div className="Circles">
-            
+            <div className="">
+
+            </div>
         </div>
     )
 }
